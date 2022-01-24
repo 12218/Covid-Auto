36746c591539265bbf2122f12ab29575d7f97602
12218 <2402767956@qq.com>
2022-01-24 19:32:21 +0800

    Django部分登录url、view创建

--- Django/CovidAuto/CovidAuto/urls.py
+++ Django/CovidAuto/CovidAuto/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from task.views import login_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_page, name = 'login_page'), # 登录页
 ]
--- Django/CovidAuto/task/views.py
+++ Django/CovidAuto/task/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import context
+from .models import Task
 
 # Create your views here.
+def login_page(request):
+    context = {}
+    return render(request, 'login.html', context)
